849bc576c57af1149d94b55d310b2670f0510512
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-01-26 19:06:29 +0530

    Intial Worrking project with well working biding.

---
+++ android/app/src/main/kotlin/com/example/flutterchatbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterchatbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+import './screen/chat_screen.dart';
+import 'screen/auth_screen.dart';
+import 'screen/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Future<FirebaseApp> initialization = Firebase.initializeApp();
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: initialization,
+        builder: (context, appSnapshot) {
+          return MaterialApp(
+            title: 'FlutterChat',
+            theme: ThemeData(
+              primaryColor: Colors.pink,
+              buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+                  .copyWith(secondary: Colors.deepPurple)
+                  .copyWith(background: Colors.pink),
+            ),
+            home: appSnapshot.connectionState != ConnectionState.done
+                ? SplashScreen()
+                : StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (ctx, userSnapshot) {
+                      if (userSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return SplashScreen();
+                      }
+                      if (userSnapshot.hasData) {
+                        return ChatScreen();
+                      }
+                      return AuthScreen();
+                    }),
+          );
+        });
+  }
+}
