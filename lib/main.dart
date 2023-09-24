c18cd031e8e0fab9231d1a460d3e3d799f47f459
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-24 16:13:06 +0530

    Making new auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'firebase_options.dart';
 import './screen/chat_screen.dart';
 import 'screen/auth_screen.dart';
 import 'screen/splash_screen.dart';
+import 'view/screen/new_login_screen.dart';
 
 late Size globalAppSize;
 Future<void> main() async {
@@ -44,65 +45,81 @@ _initializeFirebase() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Future<FirebaseApp> initialization = Firebase.initializeApp();
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: initialization,
-      builder: (context, appSnapshot) {
-        return MaterialApp(
-          title: 'FlutterChat',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            useMaterial3: false,
-            canvasColor: const Color.fromRGBO(155, 253, 180, 1),
-            primaryColor: const Color.fromRGBO(2, 141, 64, 1),
-            cardColor: const Color.fromRGBO(165, 158, 169, 0.9),
-            iconTheme: const IconThemeData(color: Colors.black),
-            textTheme: const TextTheme(
-                bodySmall: TextStyle(
-                  color: Colors.black,
-                  fontSize: 13,
-                  fontWeight: FontWeight.bold,
-                ),
-                bodyMedium: TextStyle(
-                  color: Color.fromRGBO(31, 29, 32, 1),
-                  fontSize: 13,
-                  fontWeight: FontWeight.bold,
-                )),
-            appBarTheme: const AppBarTheme().copyWith(
-              color: const Color.fromRGBO(5, 241, 64, 1),
-              titleTextStyle:
-                  const TextStyle().copyWith(color: Colors.black, fontSize: 23),
-            ),
-            buttonTheme: ButtonTheme.of(context).copyWith(
-              buttonColor: const Color.fromRGBO(217, 207, 67, 1),
-              textTheme: ButtonTextTheme.primary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
-          home: appSnapshot.connectionState != ConnectionState.done
-              ? SplashScreen()
-              : StreamBuilder(
-                  stream: FirebaseAuth.instance.authStateChanges(),
-                  builder: (ctx, userSnapshot) {
-                    if (userSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return SplashScreen();
-                    }
-                    if (userSnapshot.hasData) {
-                      return ChatScreen();
-                    }
-                    return AuthScreen();
-                  },
-                ),
-        );
-      },
+    return MaterialApp(
+      title: 'FlutterChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: const NewLoginScreen(),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final Future<FirebaseApp> initialization = Firebase.initializeApp();
+//     return FutureBuilder(
+//       // Initialize FlutterFire:
+//       future: initialization,
+//       builder: (context, appSnapshot) {
+//         return MaterialApp(
+//           title: 'FlutterChat',
+//           debugShowCheckedModeBanner: false,
+//           theme: ThemeData(
+//             useMaterial3: false,
+//             canvasColor: const Color.fromRGBO(155, 253, 180, 1),
+//             primaryColor: const Color.fromRGBO(2, 141, 64, 1),
+//             cardColor: const Color.fromRGBO(165, 158, 169, 0.9),
+//             iconTheme: const IconThemeData(color: Colors.black),
+//             textTheme: const TextTheme(
+//                 bodySmall: TextStyle(
+//                   color: Colors.black,
+//                   fontSize: 13,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//                 bodyMedium: TextStyle(
+//                   color: Color.fromRGBO(31, 29, 32, 1),
+//                   fontSize: 13,
+//                   fontWeight: FontWeight.bold,
+//                 )),
+//             appBarTheme: const AppBarTheme().copyWith(
+//               color: const Color.fromRGBO(5, 241, 64, 1),
+//               titleTextStyle:
+//                   const TextStyle().copyWith(color: Colors.black, fontSize: 23),
+//             ),
+//             buttonTheme: ButtonTheme.of(context).copyWith(
+//               buttonColor: const Color.fromRGBO(217, 207, 67, 1),
+//               textTheme: ButtonTextTheme.primary,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//             ),
+//           ),
+//           home: appSnapshot.connectionState != ConnectionState.done
+//               ? SplashScreen()
+//               : StreamBuilder(
+//                   stream: FirebaseAuth.instance.authStateChanges(),
+//                   builder: (ctx, userSnapshot) {
+//                     if (userSnapshot.connectionState ==
+//                         ConnectionState.waiting) {
+//                       return SplashScreen();
+//                     }
+//                     if (userSnapshot.hasData) {
+//                       return ChatScreen();
+//                     }
+//                     return AuthScreen();
+//                   },
+//                 ),
+//         );
+//       },
+//     );
+//   }
+// }
