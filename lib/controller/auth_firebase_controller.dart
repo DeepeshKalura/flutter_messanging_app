95b633cd143d480657c68312cd5e358e3d119dd5
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-28 12:46:41 +0530

    update the auth email code

--- lib/controller/auth_firebase_controller.dart
+++ lib/controller/auth_firebase_controller.dart
@@ -1,16 +1,24 @@
 import 'dart:developer' as developer;
+import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'connectivity_controller.dart';
+import 'random_avator_api_controller.dart';
+import 'stroage_firebase_controller.dart';
 
 class AuthFirebaseController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final ConnectivityController _connectivityController;
-  AuthFirebaseController(this._connectivityController);
+  late StorageFirebaseController storageFirebaseController;
+
+  AuthFirebaseController(this._connectivityController) {
+    storageFirebaseController =
+        StorageFirebaseController(_connectivityController);
+  }
 
   // Register a user with email and password and send a verification email
-  Future<User?> registerWithEmailAndPassword(
+  Future<String> registerWithEmailAndPassword(
       String email, String password) async {
     try {
       bool hasInternet =
@@ -25,16 +33,23 @@ class AuthFirebaseController {
         password: password,
       );
 
+      Uint8List file = await RandomImageAPIController.getRandomAvator();
       // Send a verification email to the user
+      final String downloadUrl = await storageFirebaseController.uploadFile(
+        "profile/${userCredential.user!.uid}",
+        file,
+        "SvgFile",
+      );
+      await userCredential.user!.updatePhotoURL(downloadUrl);
       await userCredential.user!.sendEmailVerification();
 
-      return userCredential.user;
+      return "sent verification email successfully";
     } on FirebaseAuthException catch (error) {
       developer.log("Error during registration: $error");
-      return null;
+      return error.message!;
     } catch (error) {
       developer.log("Error during registration: $error");
-      return null;
+      return error.toString();
     }
   }
 
@@ -53,7 +68,18 @@ class AuthFirebaseController {
         email: email,
         password: password,
       );
-      return userCredential.user;
+      if (userCredential.user != null) {
+        // Check if the user's email is verified
+        if (userCredential.user!.emailVerified) {
+          return userCredential.user;
+        } else {
+          developer.log("Email not verified");
+          return null;
+        }
+      } else {
+        developer.log("User is null");
+        return null;
+      }
     } catch (error) {
       developer.log("Error during sign-in: $error");
       return null;
